9e3e441288c1a729fa5f77a95a5064240e94bb3b
Vidushi Sharma <vidushi1000@gmail.com>
2017-07-25 14:29:04 -0400

    Starts the FCM

---
+++ friendly_computing_machine/math.py
@@ -0,0 +1,22 @@
+"""
+A small set of functions for doing math operations.
+"""
+
+#Write a function named add that adds two values
+def add(arg1, arg2):
+    """
+    Function that adds two arguments
+    """
+    return  2 + 5
+
+def mult(arg1, arg2):
+    """
+    Function that multiplies two arguments"
+    """
+    return 2 * 5
+
+def sub(arg1, arg2):
+    """
+    Function that subtracts arg2 from arg1
+    """
+    return arg1 - arg2
