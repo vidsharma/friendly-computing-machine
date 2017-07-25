84819535903eea65c19715bf007e79e960e2da78
Vidushi Sharma <vidushi1000@gmail.com>
2017-07-25 15:20:20 -0400

    Added tests

--- tests/test_math.py
+++ tests/test_math.py
@@ -8,3 +8,7 @@ import pytest
 def test_add():
     assert fcm.math.add(5, 2) == 7
     assert fcm.math.add(2, 5) == 7
+    assert fcm.math.add(1, 2) == 3
+
+def test_mult():
+    assert fcm.math.mult(2, 5) == 10 
